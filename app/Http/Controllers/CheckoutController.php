dafb07139f413e593a69c818bdeb1e0a7d33dd4a
Tejas Soni <tejas.soni@sinelogix.com>
2022-03-01 11:51:21 +0530

    Stripe Payment : Initial commit

---
+++ app/Http/Controllers/CheckoutController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CheckoutController extends Controller
+{
+    public function checkout()
+    {
+        // Enter Your Stripe Secret
+        \Stripe\Stripe::setApiKey(env("STRIPE_SECRET", "somedefaultvalue"));
+        
+        $amount = 100;
+        $amount *= 100;
+        $amount = (int) $amount;
+
+        $payment_intent = \Stripe\PaymentIntent::create([
+            'description' => 'Stripe Test Payment',
+            'amount' => $amount,
+            'currency' => 'INR',
+            'description' => 'Payment From Codehunger',
+            'payment_method_types' => ['card'],
+        ]);
+        $intent = $payment_intent->client_secret;
+
+        return view('checkout.credit-card', compact('intent'));
+    }
+
+    public function afterPayment()
+    {
+        echo 'Payment Has been Received';
+    }
+}
